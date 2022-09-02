4e430b8fc835027a324ab0be4b03f2d941f4931a
Marcus <hymanmarcus95@gmail.com>
2022-09-02 11:36:55 -0400

    finish 2b

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
-
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board, turn , isWinner
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.querytSelector("block")
+const messageEl = document.querySelector("#message")
+//// const section = document.quertSelector("board")
+//// const = document.quertSelector("")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -16,3 +18,5 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+console.log(squareEls)
+console.log(messageEl)
