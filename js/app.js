068bdfa589f1eae671d68d47dd123b722a0fc5af
Marcus <hymanmarcus95@gmail.com>
2022-09-06 20:39:21 -0400

    fix handle function

--- js/app.js
+++ js/app.js
@@ -1,66 +1,89 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const squareEls = document.querySelector("block")
-const messageEl = document.querySelector("#message")
-const wiiningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
 
-/*---------------------------- Variables (state) ----------------------------*/
-
-let board, turn, winner
-
-/*------------------------ Cached Element References ------------------------*/
-
-//// const section = document.quertSelector("board")
-//// const = document.quertSelector("")
-// console.log(squareEls)
-// console.log(messageEl)
-let gameBoard = document.querySelector("seciton")
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-gameBoard.addEventListener('click',handleClick)
-
-/*-------------------------------- Functions --------------------------------*/
-
-init ()
-
-function init() {
-  let board = [null, null, null, null, null, null, null, null, null,]
-  let turn = 1
-  let Winner = null
-  render ()
-}
-// console.log(init)
-
-
-function render() {
-  board.forEach(function(value, index) {
-    if (value === 1) { 
-      squareEls[index].innnerText = " X "
-    } else if (value === -1) {
-      squareEls[index].innnerText = " O "
-    } else (value === null)
-      squareEls[index].innnerText + " "
-}
-
-    if (winner === null) {
-      messageEl.textContent = "It is Player ${turn}'s turn."
-  } else if (winner === 'T') {
-    messageEl.textContent = "The game is a tie!"
-  } else {
-    messageEl.textContent = "Congrats! Player ${winner} is the winner!"
+const winningCombos = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+  
+  /*---------------------------- Variables (state) ----------------------------*/
+  let winner, turn, board;
+  
+  
+  /*------------------------ Cached Element References ------------------------*/
+  
+  const squareEls = document.querySelectorAll(".grid-block")
+  const messageEls = document.querySelector("#message")
+  const parentEl = document.querySelector(".board")
+  const resetBtnEl = document.querySelector("#reset-button")
+  /*----------------------------- Event Listeners -----------------------------*/
+  
+  
+  parentEl.addEventListener('click', handleClick)//this uses the bubbling method
+  resetBtnEl.addEventListener('click', init)
+  
+  
+  
+  
+  
+  /*-------------------------------- Functions --------------------------------*/
+  init()
+  
+  function init() {
+  board = [null,null,null,null,null,null,null,null,null]
+  turn = 1
+  winner = null
+  render()
   }
-}
-
-function handleClick(event) {
-const sqIdx
-}
+  
+  function render() {
+    board.forEach((value, idx) => {
+      let playerChoice = squareEls[idx]
+      if (value === null) {
+        playerChoice.textContent = ""
+      }
+        if (value === 1)  {
+          playerChoice.textContent= 'X' 
+        } 
+        if  (value === -1) {
+          playerChoice.textContent = 'O'
+        }
+    })
+    if (!winner) {
+      messageEls.textContent = `Your turn player ${turn === 1 ? "X" : "O"}`
+    } else if (winner === "T") {
+      messageEls.textContent = "The game is a tie"
+    } else {
+      messageEls.textContent = `Congrats Player ${winner === 1 ? "X" : "O"} You Won`
+    }
+  }
+  
+  function handleClick (evt) {
+    const sqIdx = parseInt(evt.target.id[2])
+    if (board[sqIdx] != null) return
+      else {
+        board[sqIdx] = turn
+        turn = turn * (-1)
+        winner = getWinner()
+        render()
+      }
+  }
+  
+  function getWinner() {
+    for (let i = 0; i < winningCombos.length; i++) {
+      if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]) === 3) return board[winningCombos[i][0]]
+    }
+      if (board.includes(null)) {
+        return 
+      } else {
+        return "T"
+      }
+  }
+  
